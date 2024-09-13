8fb20a48792ace077e8339770d16065786a0c125
huang1993 <150344345+huangkuku@users.noreply.github.com>
2024-09-13 10:44:16 +0800

    Add files via upload

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,24 @@
 ﻿// module.exports = mongoose.Model("User", userSchema)
 const User = require("../models/userModel");
-// login
+
+// login and connect database by async fun
 const loginUser = async (req, res)=>{
     res.json({mssg: "login user"})
-}
+};
 
-// signup
-const signupUser = async (req, res)=>{
-    res.json({mssg: "signup user"})
-}
+// signup 註冊
+const signupUser = async function(req, res){
+    // email, password from request body to server
+    const { email, password} = req.body;
+    // res.json({ email, password})
+    try {
+        const user = await User.signup(email, password) // use static method
+        // because 'return user' from ' userSchema.statics.signup of useModel.js '
+        // user Obj is created mongoDB
+        res.status(200).json({email, user})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+};
 
 module.exports = {loginUser, signupUser};
